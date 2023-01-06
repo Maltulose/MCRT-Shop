f24389a8bfffb2c028ed3db798a8eae106cf5c63
Christian <ch26@gmx.de>
2023-01-06 18:08:47 +0100

    Person im Winter lässt sich auswählen

--- js/navigation.js
+++ js/navigation.js
@@ -1,10 +1,16 @@
+var jahreszeiten = ["sommer", "winter", "regen", "baum", "cocktail"];  // Names of the elements
+var mausIstAufDerPerson = false
+var jahreszeit = jahreszeiten[0];
+var personenRegenkleidung = ["Regen_Colored_Hut.png", "Regen_Colored_Jacke.png", "Regen_Colored_Stiefel.png"];  // Names of the elements
+
 function setPage(page) {
     
 }
 
 function setJahreszeit(jahreszeit) {
-    var jahreszeiten = ["sommer", "winter", "regen", "baum", "cocktail"];  // Names of the elements
+    
     var personenJahreszeiten = ["Sommer_colored.png", "Winter_colored.png", "Regen_Colored_1.png", "baum", "cocktail"];  // Names of the elements
+    
 
     
 
@@ -12,8 +18,45 @@ function setJahreszeit(jahreszeit) {
         document.getElementById("BT" + element).style = "topnav";
     });
     document.getElementById("BT" + jahreszeit).style.backgroundColor = "#04AA6D";
-    document.getElementById("IVperson").src = personenJahreszeiten[jahreszeiten.findIndex(element => element == jahreszeit)];
+
+    jahreszeit = jahreszeiten.findIndex(element => element == jahreszeit);
+    this.jahreszeit = jahreszeiten[jahreszeit]
+
+    document.getElementById("IVperson").src = personenJahreszeiten[jahreszeit];
 
     console.log(personenJahreszeiten[jahreszeiten.findIndex(element => element == jahreszeit)])
 
+}
+
+function onMausUeberDerPerson(e) {
+    if (mausIstAufDerPerson == true) {
+        let bounds = document.getElementById("ROWmain").getBoundingClientRect();
+        let x = e.clientX - bounds.left;
+        let y = e.clientY - bounds.top - document.getElementById("ROWmain").offsetHeight;
+
+
+        let maxHöhe = document.getElementById("IVperson").offsetHeight;
+
+        let pixel = maxHöhe / 10;
+        let kopf = pixel * 3;
+        let bauch = kopf + pixel * 5.5;
+        let fuss = maxHöhe;
+
+        console.log(kopf);
+
+
+        if (y < kopf) {
+            setAuswahl("_Colored_Hut.png")
+        } else if (y < bauch) {
+            setAuswahl("_Colored_Jacke.png")
+        } else {
+            setAuswahl("_Colored_Stiefel.png")
+        }
+    }
+
+    function setAuswahl(region) {
+        var test = jahreszeit + region
+        console.log(test)
+        document.getElementById("IVperson").src = jahreszeit + region
+    }
 }
