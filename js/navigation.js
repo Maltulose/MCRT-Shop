e490449822c872994c49c5b74c5329439cc2e400
Christian <ch26@gmx.de>
2023-01-07 12:42:41 +0100

    Alle 3 Jahreszeiten sind funktionsfähig

--- js/navigation.js
+++ js/navigation.js
@@ -1,14 +1,12 @@
 var jahreszeiten = ["sommer", "winter", "regen"];  // Names of the elements
 var mausIstAufDerPerson = false
-var jahreszeit = jahreszeiten[0];
+var jahreszeit = 0;
 
 var personenJahreszeiten = ["Sommer_colored.png", "Winter_colored.png", "Regen_Colored_1.png"];  // Names of the elements
 
-var personenRegenkleidung = ["Regen_Colored_Hut.png", "Regen_Colored_Jacke.png", "Regen_Colored_Stiefel.png"];  // Names of the elements
+var koerperregionenY = ["KopfOben", "KopfMitte", "KopfUnten", "Bauch", "Hose", "Schuhe"]
+var koerperregionenX = ["Links", "Mitte", "Rechts"]
 
-function setPage(page) {
-    
-}
 
 function setJahreszeit(jahreszeit) {
     jahreszeiten.forEach(element => {
@@ -33,35 +31,160 @@ function onMausUeberDerPerson(e) {
 
 
         let maxHöhe = document.getElementById("IVperson").offsetHeight;
-
-        let pixel = maxHöhe / 10;
-        let kopf = pixel * 3;
-        let bauch = kopf + pixel * 5.5;
-        let fuss = maxHöhe;
+        let maxBreite = document.getElementById("IVperson").offsetWidth;
 
 
-        if (y < kopf) {
-            setAuswahl("_Colored_Hut.png")
-        } else if (y < bauch) {
-            setAuswahl("_Colored_Jacke.png")
+        let pixel = maxHöhe / 20;
+        let breite = maxBreite / 3;
+        var koerperX, koerperY;
+
+        if (x < breite) {
+            koerperX = koerperregionenX[0];
+        } else if (x < breite * 1.7) {
+            koerperX = koerperregionenX[1];
         } else {
-            setAuswahl("_Colored_Stiefel.png")
+            koerperX = koerperregionenX[2];
         }
+
+
+
+        switch (this.jahreszeit) {
+            case 0:
+                if (y < pixel * 3) {
+                    koerperY = koerperregionenY[0];
+                } else if (y < pixel * 6) {
+                    koerperY = koerperregionenY[1];
+                } else if (y < pixel * 7) {
+                    koerperY = koerperregionenY[2];
+                } else if (y < pixel * 12) {
+                    koerperY = koerperregionenY[3];
+                } else if (y < pixel * 18) {
+                    koerperY = koerperregionenY[4];
+                } else {
+                    koerperY = koerperregionenY[5];
+                }
+                break;
+            case 1:
+                if (y < pixel * 3) {
+                    koerperY = koerperregionenY[0];
+                } else if (y < pixel * 6) {
+                    koerperY = koerperregionenY[1];
+                } else if (y < pixel * 8) {
+                    koerperY = koerperregionenY[2];
+                } else if (y < pixel * 13) {
+                    koerperY = koerperregionenY[3];
+                } else if (y < pixel * 18) {
+                    koerperY = koerperregionenY[4];
+                } else {
+                    koerperY = koerperregionenY[5];
+                }
+                break;
+        
+            default:
+                if (y < pixel * 3) {
+                    koerperY = koerperregionenY[0];
+                } else if (y < pixel * 6) {
+                    koerperY = koerperregionenY[1];
+                } else if (y < pixel * 7) {
+                    koerperY = koerperregionenY[2];
+                } else if (y < pixel * 15) {
+                    koerperY = koerperregionenY[3];
+                } else if (y < pixel * 18) {
+                    koerperY = koerperregionenY[4];
+                } else {
+                    koerperY = koerperregionenY[5];
+                }
+                break;
+        }
+
+        
+
+        var bild = getPictureName(koerperX, koerperY);
+        setAuswahl(bild)
     } else {
         showBasicPerson()
     }
 }
 
+function getPictureName(xKoordinate, yKoordinate) {
+    switch (this.jahreszeit) {
+        case 0:
+            switch (yKoordinate) {
+                case koerperregionenY[0]:
+                    return jahreszeiten[jahreszeit] + "_Colored_Hut.png"
+                case koerperregionenY[1]:
+                    return jahreszeiten[jahreszeit] + "_Colored_Brille.png"
+                case koerperregionenY[2]:
+                    return jahreszeiten[jahreszeit] + "_Colored_Brille.png"
+                case koerperregionenY[3]:
+                    return jahreszeiten[jahreszeit] + "_Colored_Jacke.png"
+                case koerperregionenY[4]:
+                    return jahreszeiten[jahreszeit] + "_Colored_Hose.png"
+                case koerperregionenY[5]:
+                    return jahreszeiten[jahreszeit] + "_Colored_Stiefel.png"
+                default:
+                    break;
+            }
+        case 1:
+            switch (yKoordinate) {
+                case koerperregionenY[0]:
+                    return jahreszeiten[jahreszeit] + "_Colored_Hut.png"
+                case koerperregionenY[1]:
+                    return jahreszeiten[jahreszeit] + "_Colored_Ohrschuetzer.png"
+                case koerperregionenY[2]:
+                    return jahreszeiten[jahreszeit] + "_Colored_Schal.png"
+                case koerperregionenY[3]:
+                    
+                    return jahreszeiten[jahreszeit] + "_Colored_Jacke.png"
+                case koerperregionenY[4]:
+                    if (xKoordinate == koerperregionenX[0] || xKoordinate == koerperregionenX[2]) {
+                        return jahreszeiten[jahreszeit] + "_Colored_Handschuhe.png"
+                    }
+                    return jahreszeiten[jahreszeit] + "_Colored_Jacke.png"
+                case koerperregionenY[5]:
+                    return jahreszeiten[jahreszeit] + "_Colored_Stiefel.png"
+                default:
+                    break;
+            }
+        case 2:
+            switch (yKoordinate) {
+                case koerperregionenY[0]:
+                    return jahreszeiten[jahreszeit] + "_Colored_Hut.png"
+                case koerperregionenY[1]:
+                    return jahreszeiten[jahreszeit] + "_Colored_Hut.png"
+                case koerperregionenY[2]:
+                    return jahreszeiten[jahreszeit] + "_Colored_Jacke.png"
+                case koerperregionenY[3]:
+                    if (xKoordinate == koerperregionenX[0]) {
+                        return jahreszeiten[jahreszeit] + "_Colored_Schirm.png"
+                    }
+                    return jahreszeiten[jahreszeit] + "_Colored_Jacke.png"
+                case koerperregionenY[4]:
+                    if (xKoordinate == koerperregionenX[0]) {
+                        return jahreszeiten[jahreszeit] + "_Colored_Schirm.png"
+                    }
+                    return jahreszeiten[jahreszeit] + "_Colored_Stiefel.png"
+                case koerperregionenY[5]:
+                    if (xKoordinate == koerperregionenX[0]) {
+                        return jahreszeiten[jahreszeit] + "_Colored_Schirm.png"
+                    }
+                    return jahreszeiten[jahreszeit] + "_Colored_Stiefel.png"
+                default:
+                    break;
+            }
+        default:
+            return "error"
+    }
+}
+
 function setAuswahl(region) {
-    var pfad = "pictures/" + jahreszeiten[jahreszeit] + "/" + jahreszeiten[jahreszeit] + region
+    var pfad = "pictures/" + jahreszeiten[jahreszeit] + "/" + region
 
     document.getElementById("IVperson").src = pfad
 }
 
 function showBasicPerson() {
     var pfad = "pictures/" + jahreszeiten[jahreszeit] + "/" + personenJahreszeiten[jahreszeit]
-
-    console.log(pfad)
 
     document.getElementById("IVperson").src = pfad
     mausIstAufDerPerson = false
